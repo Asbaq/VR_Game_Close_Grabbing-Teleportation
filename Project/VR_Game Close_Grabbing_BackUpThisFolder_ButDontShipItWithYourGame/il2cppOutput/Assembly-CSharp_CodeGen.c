e027ec33b5064fdf01a0da031e9996c8cdb16572
Asbaq Laareb <asbaqlaareb45@gmail.com>
2024-08-04 19:48:12 +0530

    y
    y

---
+++ Project/VR_Game Close_Grabbing_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,89 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void Readme::.ctor()
+extern void Readme__ctor_m69C325C4C171DCB0312B646A9034AA91EA8C39C6 (void);
+// 0x00000002 System.Void Readme/Section::.ctor()
+extern void Section__ctor_m5F732533E4DFC0167D965E5F5DB332E46055399B (void);
+// 0x00000003 System.Void ActivateTeleportationRay::Update()
+extern void ActivateTeleportationRay_Update_m1B71CD9B687D962A62474CE9EA1CF83DCCA9F2E1 (void);
+// 0x00000004 System.Void ActivateTeleportationRay::.ctor()
+extern void ActivateTeleportationRay__ctor_mAF315B0B584C8A7AF8FB88A6259E0C5BFA0F22AA (void);
+// 0x00000005 System.Void AnimateHandOnInput::Update()
+extern void AnimateHandOnInput_Update_mC03CF4BE3804324BBC246D79B71F83B1A8848CDD (void);
+// 0x00000006 System.Void AnimateHandOnInput::.ctor()
+extern void AnimateHandOnInput__ctor_mEA43A853FB1CB874A55072BFBF7A8B78BEF60F1C (void);
+// 0x00000007 System.Void FireBullet::Start()
+extern void FireBullet_Start_mE0FF18DE492AF53ED714D3FDE007292AA2E5FEDD (void);
+// 0x00000008 System.Void FireBullet::Fire(UnityEngine.XR.Interaction.Toolkit.ActivateEventArgs)
+extern void FireBullet_Fire_m2C08C819EEF653568FE614B208815136AFE9AFC8 (void);
+// 0x00000009 System.Void FireBullet::.ctor()
+extern void FireBullet__ctor_m1041E61C0607825E1AED9845F7BC16393545AC5E (void);
+// 0x0000000A System.Void HMD_Info_Manager::Start()
+extern void HMD_Info_Manager_Start_m335D7714F20AC7901DB51FBE1E60B98D76E938FB (void);
+// 0x0000000B System.Void HMD_Info_Manager::Update()
+extern void HMD_Info_Manager_Update_mD00B85C3BFA8017C650B2E5656A218359BD0FD73 (void);
+// 0x0000000C System.Void HMD_Info_Manager::.ctor()
+extern void HMD_Info_Manager__ctor_mAE6D90866F4B32299296496CF770F6BBE3851B52 (void);
+static Il2CppMethodPointer s_methodPointers[12] = 
+{
+	Readme__ctor_m69C325C4C171DCB0312B646A9034AA91EA8C39C6,
+	Section__ctor_m5F732533E4DFC0167D965E5F5DB332E46055399B,
+	ActivateTeleportationRay_Update_m1B71CD9B687D962A62474CE9EA1CF83DCCA9F2E1,
+	ActivateTeleportationRay__ctor_mAF315B0B584C8A7AF8FB88A6259E0C5BFA0F22AA,
+	AnimateHandOnInput_Update_mC03CF4BE3804324BBC246D79B71F83B1A8848CDD,
+	AnimateHandOnInput__ctor_mEA43A853FB1CB874A55072BFBF7A8B78BEF60F1C,
+	FireBullet_Start_mE0FF18DE492AF53ED714D3FDE007292AA2E5FEDD,
+	FireBullet_Fire_m2C08C819EEF653568FE614B208815136AFE9AFC8,
+	FireBullet__ctor_m1041E61C0607825E1AED9845F7BC16393545AC5E,
+	HMD_Info_Manager_Start_m335D7714F20AC7901DB51FBE1E60B98D76E938FB,
+	HMD_Info_Manager_Update_mD00B85C3BFA8017C650B2E5656A218359BD0FD73,
+	HMD_Info_Manager__ctor_mAE6D90866F4B32299296496CF770F6BBE3851B52,
+};
+static const int32_t s_InvokerIndices[12] = 
+{
+	6461,
+	6461,
+	6461,
+	6461,
+	6461,
+	6461,
+	6461,
+	5094,
+	6461,
+	6461,
+	6461,
+	6461,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	12,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
